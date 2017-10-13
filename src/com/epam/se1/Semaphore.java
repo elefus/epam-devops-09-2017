d834fe549f252a050928608e192ff6fecb45bc6d
elefus <elefus@yandex.ru>
2017-10-14 00:24:44 +0300

    Занятие 13.10.2017 (7 занятие).

--- src/com/epam/se1/Semaphore.java
+++ src/com/epam/se1/Semaphore.java
@@ -8,6 +8,15 @@ public class Semaphore {
     // 10 - 13 Зеленый
     // ...
     public static void main(String[] args) {
+        Integer value = Integer.parseInt(args[0]);
+        value %= 10;
 
+        if (value >= 0 && value <= 3) {
+            System.out.println("Green");
+        } else if (value >= 4 && value <= 5) {
+            System.out.println("Yellow");
+        } else  {
+            System.out.println("Red");
+        }
     }
 }
